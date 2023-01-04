aac8ae2fd19eea1a87d132cd2144d0b2e6d84b91
Mutallib <qmutallib2002@gmail.com>
2023-01-04 20:10:38 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AddTransaction from "./components/AddTransaction";
+import Balance from "./components/Balance";
+import { GlobalProvider } from "./context/GlobalState";
+import Header from "./components/Header";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionLists from "./components/TransactionLists";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalProvider>
+        <Header />
+        <div className="container">
+          <Balance />
+          <IncomeExpenses />
+          <TransactionLists />
+          <AddTransaction />
+        </div>
+      </GlobalProvider>
+    </>
   );
-}
+};
 
 export default App;
